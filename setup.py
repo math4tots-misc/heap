b5441e9f6278f6ba3b1d7c1a2d57fe748c6c651a
math4tots <math4tots@gmail.com>
2014-05-15 22:05:10 -0700

    make setup.py happy

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
 	version = '0.1',
 	description = 'heap implementation based on heapq',
 	author = 'Kyumin Kim',
+	author_email = 'math4tots@gmail.com',
 	url = 'https://github.com/math4tots/heap',
 	download_url = 'https://github.com/math4tots/heap/tarball/0.1',
 	keywords = ['heap'],
