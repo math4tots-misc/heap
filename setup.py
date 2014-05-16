21a77b52d8edd9867104f1a00a853b34ff1cb490
math4tots <math4tots@gmail.com>
2014-05-15 22:08:42 -0700

    from heap.heap import heap; version 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
 	name = 'heap',
 	packages = ['heap'],
-	version = '0.1',
+	version = '0.2',
 	description = 'heap implementation based on heapq',
 	author = 'Kyumin Kim',
 	author_email = 'math4tots@gmail.com',
 	url = 'https://github.com/math4tots/heap',
-	download_url = 'https://github.com/math4tots/heap/tarball/0.1',
+	download_url = 'https://github.com/math4tots/heap/tarball/0.2',
 	keywords = ['heap'],
 	classifiers = []
 )
